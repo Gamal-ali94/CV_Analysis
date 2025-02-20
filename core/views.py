aa7a0296d4c350e5286373ff14337f6147e8d2d0
Gamal Ali <gamal.adbelazez94@gmail.com>
2025-02-20 14:32:58 +0400

    intital 2

--- core/views.py
+++ core/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render, redirect
+import json
+
+import openai
 from django.contrib import messages
+from django.shortcuts import redirect, render
+
 from .forms import CandidateForm, PromptForm
 from .models import Candidate
 from .ocr import extract_text_from_file
-import json
-import openai
-import os
 from .openai_services import parse_resume_with_llm
+
 
 # Create your views here.
 def upload_cv(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CandidateForm(request.POST, request.FILES)
         if form.is_valid():
             candidate = form.save()
@@ -19,14 +21,17 @@ def upload_cv(request):
             try:
                 candidate_data = json.loads(parse_resume_with_llm(extracted_text))
             except json.JSONDecodeError:
-                messages.error(request, "We encountered an issue parsing your CV. Please try again.")
+                messages.error(
+                    request,
+                    "We encountered an issue parsing your CV. Please try again.",
+                )
                 candidate.delete()
-                return redirect('upload_cv')
+                return redirect("upload_cv")
             except Exception as e:
                 messages.error(request, f"An unexpected error occurred: {str(e)}")
                 candidate.delete()
-                return redirect('upload_cv')
-            
+                return redirect("upload_cv")
+
             candidate.personal_info = candidate_data["personal_info"]
             candidate.education = candidate_data["education"]
             candidate.work_experience = candidate_data["work_experience"]
@@ -34,37 +39,52 @@ def upload_cv(request):
             candidate.projects = candidate_data["projects"]
             candidate.certificates = candidate_data["certificates"]
             candidate.save()
-            return redirect('candidate_view', pk=candidate.id)
+            return redirect("candidate_view", pk=candidate.id)
     else:
         form = CandidateForm()
 
-    return render(request, 'core/upload.html', {'form': form})
+    return render(request, "core/upload.html", {"form": form})
+
 
 def candidate_view(request, pk):
     candidate = Candidate.objects.get(pk=pk)
-    return render(request, 'core/candidate.html', {'candidate': candidate})
+    return render(request, "core/candidate.html", {"candidate": candidate})
 
 
- 
 def handle_response(request):
-    messages = request.session.get('messages', [])
-    if request.method == 'POST':
-            form = PromptForm(request.POST)
-            if form.is_valid():
-                candidate_data = Candidate.objects.values("personal_info", "education", "work_experience", "skills", "projects", "certificates")
-                prompt = form.cleaned_data['prompt']
-                messages.append({"role": "user", "content": f"Use this prompt {prompt} and an answer from {candidate_data}"})
-                response = openai.chat.completions.create(
-                    model="gpt-4o-2024-08-06",
-                    messages=messages
-                )
-                final_response = response.choices[0].message.content
-                messages.append({"role":final_response})
-                request.session['final_response'] = final_response
-                request.session['messages'] = messages
-                return redirect('chat_response')
+    messages = request.session.get("messages", [])
+    if request.method == "POST":
+        form = PromptForm(request.POST)
+        if form.is_valid():
+            candidate_data = Candidate.objects.values(
+                "personal_info",
+                "education",
+                "work_experience",
+                "skills",
+                "projects",
+                "certificates",
+            )
+            prompt = form.cleaned_data["prompt"]
+            messages.append(
+                {
+                    "role": "user",
+                    "content": f"Use this prompt {prompt} and an answer from {candidate_data}",
+                }
+            )
+            response = openai.chat.completions.create(
+                model="gpt-4o-2024-08-06", messages=messages
+            )
+            final_response = response.choices[0].message.content
+            messages.append({"role": final_response})
+            request.session["final_response"] = final_response
+            request.session["messages"] = messages
+            return redirect("chat_response")
     else:
         form = PromptForm()
-        final_response = request.sessions.get('final_response', "")
+        final_response = request.sessions.get("final_response", "")
 
-    return render(request, 'core/handle_response.html', {'form': form, 'final_response':final_response})
+    return render(
+        request,
+        "core/handle_response.html",
+        {"form": form, "final_response": final_response},
+    )
